c55c56b2f0d0fc64c50d5ba857bdecfe4999f7ee
tasos <akkgr@users.noreply.github.com>
2017-03-07 21:27:18 +0200

    init

---
+++ Startup.cs
@@ -0,0 +1,46 @@
+﻿using Hangfire;
+using Hangfire.Mongo;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace newdot
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {            
+            services.AddHangfire(x=>x.UseMongoStorage("mongodb://localhost","logs"));
+            services.AddMvc();
+        }
+                
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddSerilog();
+
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+
+            app.UseMvc();
+        }
+    }
+}
