6aad76b7635f290ee950d1a34d7da5fba2157eba
tasos <akkgr@users.noreply.github.com>
2017-03-11 11:26:52 +0200

    ws

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,7 @@
-﻿using Hangfire;
+﻿using System;
+using ChatApplication;
+using Cinnamon.Auth;
+using Hangfire;
 using Hangfire.Mongo;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -6,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using WebSocketManager;
 
 namespace newdot
 {
@@ -26,21 +30,44 @@ namespace newdot
         }
 
         public IConfigurationRoot Configuration { get; }
-        
+
         public void ConfigureServices(IServiceCollection services)
-        {            
-            services.AddHangfire(x=>x.UseMongoStorage("mongodb://localhost","logs"));
+        {
+            var dbConection = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddIdentityServer()
+                .AddTemporarySigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
+
+            services.AddHangfire(x => x.UseMongoStorage(dbConection, "logs"));
+            services.AddWebSocketManager();
             services.AddMvc();
         }
-                
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider serviceProvider)
         {
             loggerFactory.AddSerilog();
+
+            app.UseIdentityServer();
+
+            app.UseIdentityServerAuthentication(new IdentityServerAuthenticationOptions
+            {
+                Authority = "http://localhost:5000",
+                RequireHttpsMetadata = false,
+
+                ApiName = "api1"
+            });
 
             app.UseHangfireServer();
             app.UseHangfireDashboard();
 
+            app.UseWebSockets();
+
+            app.MapWebSocketManager("/ws", serviceProvider.GetService<ChatMessageHandler>());
+
             app.UseMvc();
+            app.UseStaticFiles();
         }
     }
 }
